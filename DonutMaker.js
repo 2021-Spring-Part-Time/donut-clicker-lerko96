2a237949d60e02959f1fa8d6ba1fcf740b134848
Tyler Koenig <tylerkng96@icloud.com>
2021-06-24 22:59:01 -0400

    buy autoClicker test

--- DonutMaker.js
+++ DonutMaker.js
@@ -1,22 +1,49 @@
 class DonutMaker {
-    constructor(donutCount, clickerCount, clickerCost) {
-        this.donutCount = donutCount;
-        this.clickerCount = clickerCount;
-        this.clickerCost = clickerCost;
-    }
+	constructor(donutCount, clickerCount, clickerCost) {
+		this.donutCount = donutCount;
+		this.clickerCount = clickerCount;
+		this.clickerCost = clickerCost;
+	}
 
-    click() {
-        this.donutCount += 1;
-    }
+	click() {
+		this.donutCount += 1;
+	}
 
-    addClicker() {
-        this.clickerCount += 1;
-        this.donutCount -= 100;
-        this.clickerCost *= 1.1;
-    }
+	addClicker() {
+        //Clicker count
+		// this.clickerCount += 1;
+        // if (this.donutCount >= this.clickerCost) {
+        //     return this.clickerCount += 1;
+        // }   else if (this.donutCount < this.clickerCost) {
+        //     return this.clickerCount += 0;
+        // }
+        // return this.clickerCount 
+
+        
+        //Donut 
+		this.donutCount -= this.clickerCost;
+        
+		if (this.clickerCost < (this.clickerCost * 0.1) + this.clickerCost) {
+            this.clickerCost = (this.clickerCost * 0.1) + this.clickerCost; 
+            if (this.donutCount >= this.clickerCost) {
+                return this.clickerCount += 1;
+            }   else if (this.donutCount < this.clickerCost) {
+                return this.clickerCount += 0;
+            }
+            return this.clickerCount;
+		}
+		return this.clickerCost;
+	}
     
+    // function buyClicker(this.clickerCount) {
+    //     if (this.donutCount < this.clickerCost) {
+    //         this.clickerCount+=0
+    //     }
+    //     else (this.donutCount >= this.clickerCost) {
+    //         this.clickerCount += 1;
+    //     }
+        
+    // }
 }
-
-
 
 export default DonutMaker;
--- DonutMaker.test.js
+++ DonutMaker.test.js
@@ -1,43 +1,57 @@
 // import { expect, test } from '@jest/globals';
 import DonutMaker from './DonutMaker';
 
-describe('DonutMaker', () => {
-    test('does the donutCount increase when clicked', () => {
+describe('1.1 DonutMaker', () => {
+    test('A) increase donutCount by 1 under click()', () => {
         const underTest = new DonutMaker(0, 0, 100);
         underTest.click();
         expect(underTest.donutCount).toEqual(1);
     });
 
-    test('does it return a count', () => {
+    test('B) return donutCount', () => {
         const underTest = new DonutMaker(50, 0, 0);
         expect(typeof underTest.donutCount).toMatch('number');
     });
 });
 
-describe('AutoClicker', () => {
-    test('does it return a count', () => {
-        const underTest = new DonutMaker(0, 0, 100);
+describe('1.2 AutoClicker', () => {
+    test('1.2.1 does AutoClicker return a count', () => {
+        const underTest = new DonutMaker(110, 0, 100);
         underTest.addClicker();
         expect(underTest.clickerCount).toEqual(1);
     });
 
-    test('can I add to the clickerCount', () => {
-        const underTest = new DonutMaker(100, 5, 500);
+    test('B) ability to  add to the clickerCount', () => {
+        const underTest = new DonutMaker(600, 5, 500);
         underTest.addClicker();
         expect(underTest.clickerCount).toEqual(6);
     })
 
-    test('does it subtract the auto clicker cost from the donut count', () => {
+    test('C) does .addClicker subtract the AutoClicker cost from  donutCount', () => {
         const underTest = new DonutMaker(100, 100, 100);
         underTest.addClicker();
         expect(underTest.donutCount).toEqual(0);
     })
 });
 
-describe('Auto Clicker Cost', () => {
-    test('does the clicker cost increase by 10% after purchase', () => {
+describe('1.3 AutoClicker Cost', () => {
+    test('A) does the clicker cost increase by 10% after first purchase', () => {
         const underTest = new DonutMaker(100, 0, 100);
         underTest.addClicker();
         expect(underTest.clickerCost).toEqual(110);
+    })
+
+    test('B) does the clicker cost increase by 10% after each purchase', () => {
+        const underTest = new DonutMaker(50, 1, 121);
+        underTest.addClicker();
+        expect(underTest.clickerCost).toEqual(133.1);
+    })
+})
+
+describe('1.4 Buy AutoClicker', () => {
+    test('1.4.1 Can buy AutoClicker ONLY when user has enough in donutCount', () => {
+        const underTest = new DonutMaker(99, 0, 100);
+        underTest.addClicker();
+        expect(underTest.clickerCount).toEqual(0);
     })
 })
