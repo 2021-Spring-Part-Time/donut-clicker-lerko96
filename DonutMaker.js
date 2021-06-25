8bdb654d12749752402a0406e8bbb58734212766
Tyler Koenig <tylerkng96@icloud.com>
2021-06-24 20:03:01 -0400

    testing autoclicker cost

--- DonutMaker.js
+++ DonutMaker.js
@@ -1,12 +1,22 @@
 class DonutMaker {
-    constructor(donutCount) {
+    constructor(donutCount, clickerCount, clickerCost) {
         this.donutCount = donutCount;
+        this.clickerCount = clickerCount;
+        this.clickerCost = clickerCost;
     }
 
     click() {
         this.donutCount += 1;
     }
 
+    addClicker() {
+        this.clickerCount += 1;
+        this.donutCount -= 100;
+        this.clickerCost *= 1.1;
+    }
+    
 }
+
+
 
 export default DonutMaker;
--- DonutMaker.test.js
+++ DonutMaker.test.js
@@ -3,13 +3,41 @@ import DonutMaker from './DonutMaker';
 
 describe('DonutMaker', () => {
     test('does the donutCount increase when clicked', () => {
-        const underTest = new DonutMaker(0);
+        const underTest = new DonutMaker(0, 0, 100);
         underTest.click();
         expect(underTest.donutCount).toEqual(1);
     });
 
     test('does it return a count', () => {
-        const underTest = new DonutMaker(50);
+        const underTest = new DonutMaker(50, 0, 0);
         expect(typeof underTest.donutCount).toMatch('number');
     });
 });
+
+describe('AutoClicker', () => {
+    test('does it return a count', () => {
+        const underTest = new DonutMaker(0, 0, 100);
+        underTest.addClicker();
+        expect(underTest.clickerCount).toEqual(1);
+    });
+
+    test('can I add to the clickerCount', () => {
+        const underTest = new DonutMaker(100, 5, 500);
+        underTest.addClicker();
+        expect(underTest.clickerCount).toEqual(6);
+    })
+
+    test('does it subtract the auto clicker cost from the donut count', () => {
+        const underTest = new DonutMaker(100, 100, 100);
+        underTest.addClicker();
+        expect(underTest.donutCount).toEqual(0);
+    })
+});
+
+describe('Auto Clicker Cost', () => {
+    test('does the clicker cost increase by 10% after purchase', () => {
+        const underTest = new DonutMaker(100, 0, 100);
+        underTest.addClicker();
+        expect(underTest.clickerCost).toEqual(110);
+    })
+})
