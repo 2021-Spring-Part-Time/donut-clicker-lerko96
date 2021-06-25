4e766a5a204302c0a01e7dd1cc8f0b0baf690832
Tyler Koenig <tylerkng96@icloud.com>
2021-06-25 11:03:57 -0400

    starting donut cost problem

--- DonutMaker.js
+++ DonutMaker.js
@@ -10,40 +10,18 @@ class DonutMaker {
 	}
 
 	addClicker() {
-        //Clicker count
-		// this.clickerCount += 1;
-        // if (this.donutCount >= this.clickerCost) {
-        //     return this.clickerCount += 1;
-        // }   else if (this.donutCount < this.clickerCost) {
-        //     return this.clickerCount += 0;
-        // }
-        // return this.clickerCount 
-
-        
-        //Donut 
-		this.donutCount -= this.clickerCost;
-        
 		if (this.clickerCost < (this.clickerCost * 0.1) + this.clickerCost) {
             this.clickerCost = (this.clickerCost * 0.1) + this.clickerCost; 
+            this.donutCount -= this.clickerCost;
             if (this.donutCount >= this.clickerCost) {
-                return this.clickerCount += 1;
-            }   else if (this.donutCount < this.clickerCost) {
+                return this.clickerCount++;
+            }   else if (this.donutCount < this.clickerCost) { 
                 return this.clickerCount += 0;
             }
             return this.clickerCount;
 		}
 		return this.clickerCost;
 	}
-    
-    // function buyClicker(this.clickerCount) {
-    //     if (this.donutCount < this.clickerCost) {
-    //         this.clickerCount+=0
-    //     }
-    //     else (this.donutCount >= this.clickerCost) {
-    //         this.clickerCount += 1;
-    //     }
-        
-    // }
 }
 
 export default DonutMaker;
--- DonutMaker.test.js
+++ DonutMaker.test.js
@@ -2,13 +2,13 @@
 import DonutMaker from './DonutMaker';
 
 describe('1.1 DonutMaker', () => {
-    test('A) increase donutCount by 1 under click()', () => {
+    test('1.1.1 Increase donutCount by 1 under click()', () => {
         const underTest = new DonutMaker(0, 0, 100);
         underTest.click();
         expect(underTest.donutCount).toEqual(1);
     });
 
-    test('B) return donutCount', () => {
+    test('1.1.2 Return donutCount', () => {
         const underTest = new DonutMaker(50, 0, 0);
         expect(typeof underTest.donutCount).toMatch('number');
     });
@@ -16,18 +16,18 @@ describe('1.1 DonutMaker', () => {
 
 describe('1.2 AutoClicker', () => {
     test('1.2.1 does AutoClicker return a count', () => {
-        const underTest = new DonutMaker(110, 0, 100);
+        const underTest = new DonutMaker(110, 1, 100);
         underTest.addClicker();
         expect(underTest.clickerCount).toEqual(1);
     });
 
-    test('B) ability to  add to the clickerCount', () => {
-        const underTest = new DonutMaker(600, 5, 500);
+    test('1.2.2 ability to  add to the clickerCount', () => {
+        const underTest = new DonutMaker(300, 1, 110);
         underTest.addClicker();
-        expect(underTest.clickerCount).toEqual(6);
+        expect(underTest.clickerCount).toEqual(2);
     })
 
-    test('C) does .addClicker subtract the AutoClicker cost from  donutCount', () => {
+    test('1.2.3 does .addClicker subtract the AutoClicker cost from  donutCount', () => {
         const underTest = new DonutMaker(100, 100, 100);
         underTest.addClicker();
         expect(underTest.donutCount).toEqual(0);
@@ -35,13 +35,13 @@ describe('1.2 AutoClicker', () => {
 });
 
 describe('1.3 AutoClicker Cost', () => {
-    test('A) does the clicker cost increase by 10% after first purchase', () => {
+    test('1.3.1 does the clicker cost increase by 10% after first purchase', () => {
         const underTest = new DonutMaker(100, 0, 100);
         underTest.addClicker();
         expect(underTest.clickerCost).toEqual(110);
     })
 
-    test('B) does the clicker cost increase by 10% after each purchase', () => {
+    test('1.3.2 does the clicker cost increase by 10% after each purchase', () => {
         const underTest = new DonutMaker(50, 1, 121);
         underTest.addClicker();
         expect(underTest.clickerCost).toEqual(133.1);
@@ -50,8 +50,14 @@ describe('1.3 AutoClicker Cost', () => {
 
 describe('1.4 Buy AutoClicker', () => {
     test('1.4.1 Can buy AutoClicker ONLY when user has enough in donutCount', () => {
-        const underTest = new DonutMaker(99, 0, 100);
+        const underTest = new DonutMaker(250, 0, 100);
+        underTest.addClicker();
+        expect(underTest.clickerCount).toEqual(1);
+    })
+    test('1.4.2 Can NOT buy AutoClicker when user doesnt have enough in donutCount', () => {
+        const underTest = new DonutMaker(200, 0, 91);
         underTest.addClicker();
         expect(underTest.clickerCount).toEqual(0);
     })
+
 })
